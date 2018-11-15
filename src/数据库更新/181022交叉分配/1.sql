0a7c691359a00d50db88fae909b3f1dc4f13df05
lei <2391923850@qq.com>
2018-11-15 17:07:33 +0800

    湖北交叉评查新建表

--- src/数据库更新/181022交叉分配/1.sql
+++ src/数据库更新/181022交叉分配/1.sql
@@ -36,3 +36,66 @@ BEGIN
 
   END LOOP;
 END;
+
+-- Create table
+create table YX_PC_JXPCAJ
+(
+  DWBM     CHAR(6),
+  DWMC     VARCHAR2(300),
+  BMSAH    VARCHAR2(300),
+  AJMC     VARCHAR2(300 CHAR),
+  PCFLBM   CHAR(3),
+  AJLB_BM  CHAR(4),
+  AJLB_MC  VARCHAR2(300),
+  JSDW     CHAR(6),
+  JSDWMC   VARCHAR2(300),
+  CJSJ     DATE default SYSDATE,
+  SM       VARCHAR2(3000),
+  CZR_DWBM CHAR(6),
+  CZR_DWMC VARCHAR2(300),
+  CZR_GH   CHAR(4),
+  CZR_MC   VARCHAR2(60)
+)
+tablespace ZLPC
+pctfree 10
+initrans 1
+maxtrans 255
+storage
+(
+initial 64
+next 8
+minextents 1
+maxextents unlimited
+);
+-- Add comments to the columns
+comment on column YX_PC_JXPCAJ.DWBM
+is '承办单位';
+comment on column YX_PC_JXPCAJ.DWMC
+is '被评查案件所属单位名称';
+comment on column YX_PC_JXPCAJ.BMSAH
+is '被评查案件部门受案号';
+comment on column YX_PC_JXPCAJ.AJMC
+is '被评查案件名称';
+comment on column YX_PC_JXPCAJ.PCFLBM
+is '评查类别编码';
+comment on column YX_PC_JXPCAJ.AJLB_BM
+is '被评查案件类别编码';
+comment on column YX_PC_JXPCAJ.AJLB_MC
+is '被评查案件类别名称';
+comment on column YX_PC_JXPCAJ.JSDW
+is '接受单位';
+comment on column YX_PC_JXPCAJ.JSDWMC
+is '接受单位名称';
+comment on column YX_PC_JXPCAJ.CJSJ
+is '分配时间';
+comment on column YX_PC_JXPCAJ.SM
+is '说明';
+comment on column YX_PC_JXPCAJ.CZR_DWBM
+is '分配人单位';
+comment on column YX_PC_JXPCAJ.CZR_DWMC
+is '分配人单位名称';
+comment on column YX_PC_JXPCAJ.CZR_GH
+is '分配人工号';
+comment on column YX_PC_JXPCAJ.CZR_MC
+is '分配人姓名';
+
